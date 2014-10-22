07a0a37ce5de3e411102abc0fd53797821d41815
Adam Luzsi <adam.luzsi@ppt-consulting.net>
2014-10-22 11:39:05 +0200

    update test for check always the lastest poc

--- test/test_poc_generation.rb
+++ test/test_poc_generation.rb
@@ -19,29 +19,31 @@ describe 'POCTest' do
     rescue;nil
     end.must_be_instance_of TrueClass
 
-    Dir.glob( File.join __dir__,'poc','*.{yml,yaml}' ).each do |path|
-      poc_object = YAML.load File.read(path)
+    path = Dir.glob( File.join __dir__,'poc','*.{yml,yaml}' ).sort{
+        |a,b| a.split(File::Separator)[-1].split('.')[0] <=> b.split(File::Separator)[-1].split('.')[0]
+    }[-1]
 
-      poc_object.must_be_instance_of Hash
-      poc_object.each_pair do |endpoint,options|
+    poc_object = YAML.load File.read(path)
+    poc_object.must_be_instance_of Hash
+    poc_object.each_pair do |endpoint,options|
 
-        endpoint.must_be_instance_of String
-        endpoint[0].must_be :==, '/'
+      endpoint.must_be_instance_of String
+      endpoint[0].must_be :==, '/'
 
-        options.must_be_instance_of Hash
-        options.keys.first.must_be_instance_of String
-        options.values.each do |rest_options|
-          rest_options.must_be_instance_of Hash
-          rest_options['response'].must_be_instance_of Hash
-          rest_options['response']['body'].wont_be :==, nil
-          rest_options['response']['status'].class.must_be :<=,Numeric
-          rest_options['response']['format'].must_be_instance_of String
-          rest_options['request']['query'].must_be_instance_of String
-
-        end
-
+      options.must_be_instance_of Hash
+      options.keys.first.must_be_instance_of String
+      options.values.each do |rest_options|
+        rest_options.must_be_instance_of Hash
+        rest_options['response'].must_be_instance_of Hash
+        rest_options['response']['body'].wont_be :==, nil
+        rest_options['response']['status'].class.must_be :<=,Numeric
+        rest_options['response']['format'].must_be_instance_of String
+        rest_options['request']['query']['raw'].must_be_instance_of String
+        rest_options['request']['query']['object'].must_be_instance_of Hash
 
       end
+
+
 
     end
 
